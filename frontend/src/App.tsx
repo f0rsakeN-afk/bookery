6b30cfc713e1a588e3c2139ee366635f7001f669
f0rsakeN-afk <nar3sh0@gmail.com>
2025-04-23 07:31:20 +0545

    Added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,35 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loader from "./components/shared/Loader";
 import Layout from "./components/shared/Layout";
+import ScrollToTop from "./components/shared/ScrollToTop";
 const About = lazy(() => import("./pages/About"));
 const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
 const Contact = lazy(() => import("./pages/Contact"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
-const Hero = lazy(() => import("./pages/Hero"));
+const Hero = lazy(() => import("./pages/Home"));
+const Search = lazy(() => import("./pages/Search"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Wishlist = lazy(() => import("./pages/Wishlist"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
 
 const App = () => {
   return (
     <Suspense fallback={<Loader />}>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Hero />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/Search" element={<Search />} />
+            <Route path="/wishlist" element={<Wishlist />} />
           </Route>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
