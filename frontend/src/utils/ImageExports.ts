4cdd1556c8555eb599013f6a2cb6fb8c4c50d038
f0rsakeN-afk <nar3sh0@gmail.com>
2025-04-26 17:37:53 +0545

    added image imports

--- frontend/src/utils/ImageExports.ts
+++ frontend/src/utils/ImageExports.ts
@@ -1,3 +1,8 @@
 import LogoImage from "../assets/logo.webp";
+import HeroImage from "../assets/hero.webp";
+import aboutImage from "../assets/about.avif";
+import about2Image from "../assets/about2.avif";
+import cart from "../assets/cart.svg";
+import carttransparent from "../assets/carttransparent.png";
 
-export { LogoImage };
+export { LogoImage, HeroImage, aboutImage, about2Image, cart, carttransparent };
