3b0a5a603b82ce473d9fedf6b269b245c4752a58
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-01 22:40:14 +0545

    updatedd image exports

--- frontend/src/utils/ImageExports.ts
+++ frontend/src/utils/ImageExports.ts
@@ -4,5 +4,6 @@ import aboutImage from "../assets/about.avif";
 import about2Image from "../assets/about2.avif";
 import cart from "../assets/cart.svg";
 import carttransparent from "../assets/carttransparent.png";
+import empty_wishlist from '../assets/empty-wishlist.svg'
 
-export { LogoImage, HeroImage, aboutImage, about2Image, cart, carttransparent };
+export { LogoImage, HeroImage, aboutImage, about2Image, cart, carttransparent,empty_wishlist };
