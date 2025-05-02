e9cd242f4dcbee8e5ffe5ece7164e9f3f27834b4
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-02 16:06:56 +0545

    added images

--- frontend/src/utils/ImageExports.ts
+++ frontend/src/utils/ImageExports.ts
@@ -4,6 +4,18 @@ import aboutImage from "../assets/about.avif";
 import about2Image from "../assets/about2.avif";
 import cart from "../assets/cart.svg";
 import carttransparent from "../assets/carttransparent.png";
-import empty_wishlist from '../assets/empty-wishlist.svg'
+import empty_wishlist from "../assets/empty-wishlist.svg";
+import emptyCart from "../assets/emptyCart.png";
+import searchImage from "../assets/search.png";
 
-export { LogoImage, HeroImage, aboutImage, about2Image, cart, carttransparent,empty_wishlist };
+export {
+  LogoImage,
+  HeroImage,
+  aboutImage,
+  about2Image,
+  cart,
+  carttransparent,
+  empty_wishlist,
+  emptyCart,
+  searchImage,
+};
