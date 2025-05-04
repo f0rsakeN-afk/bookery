1bdae99e66e2d258413f19a8fffb3a1bb6558839
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-04 21:18:07 +0545

    updated emptycart comp

---
+++ frontend/src/components/cart/EmptyCart.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { motion } from "motion/react";
+import { useNavigate } from "react-router-dom";
+import { emptyCart } from "@/utils/ImageExports";
+import { Button } from "../ui/button";
+
+const EmptyCart: React.FC = () => {
+  const navigate = useNavigate();
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.95 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 1 }}
+      className="flex flex-col items-center justify-center py-16 font-inter text-center"
+    >
+      <motion.img
+        src={emptyCart}
+        alt="Empty Cart"
+        className="w-44 h-44 mb-4"
+        whileHover={{
+          scale: 1.2,
+          transition: { duration: 1 },
+        }}
+      />
+      <h2 className="text-2xl font-bold mb-2">Your cart is empty</h2>
+      <p className="text-gray-500 mb-2">
+        Start adding items to your cart to make a purchase.
+      </p>
+      <Button onClick={() => navigate("/")}>Browse products</Button>
+    </motion.div>
+  );
+};
+
+export default EmptyCart;
