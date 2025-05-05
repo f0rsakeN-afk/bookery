54cc6dcdde067fd46e4a30bef48a8df4ed9688a0
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-05 18:21:15 +0545

    added wishlist services

---
+++ frontend/src/services/wishlist.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axiosInstance from "./axios";
+import toast from "react-hot-toast";
+
+async function getWishlist() {
+  const response = await axiosInstance.get(``);
+  return response.data;
+}
+
+export function useGetWishlist() {
+  return useQuery({
+    queryKey: ["wishlist"],
+    queryFn: getWishlist,
+  });
+}
+
+async function addToWishlist() {
+  const response = await axiosInstance.post(``);
+  return response.data;
+}
+
+export function useAddToWishlist() {
+  return useMutation({
+    mutationFn: addToWishlist,
+    onSuccess: (data) => {
+      toast.success(data.message || "Added to wishlist successfully");
+    },
+    onError: (error) => {
+      toast.error(error.message || "Failed adding to the wishlist");
+    },
+  });
+}
