d566327637bf41491788bd759588beca0643d890
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-08 08:39:53 +0545

    added new image

--- frontend/src/utils/ImageExports.ts
+++ frontend/src/utils/ImageExports.ts
@@ -1,5 +1,9 @@
 import LogoImage from "../assets/logo.webp";
 import HeroImage from "../assets/hero.webp";
+import HeroImage1 from "../assets/hero.png";
+import HeroImage2 from "../assets/hero1.png";
+import HeroImage3 from "../assets/hero2.png";
+import HeroImage4 from "../assets/hero3.png";
 import aboutImage from "../assets/about.avif";
 import about2Image from "../assets/about2.avif";
 import cart from "../assets/cart.svg";
@@ -7,10 +11,20 @@ import carttransparent from "../assets/carttransparent.png";
 import empty_wishlist from "../assets/empty-wishlist.svg";
 import emptyCart from "../assets/emptyCart.png";
 import searchImage from "../assets/search.png";
+import feed1 from "../assets/feed1.png";
+import feed2 from "../assets/feed2.png";
+import feed3 from "../assets/feed3.png";
+import feed4 from "../assets/feed4.png";
+import feed5 from "../assets/feed5.png";
+import feed6 from "../assets/feed6.png";
 
 export {
   LogoImage,
   HeroImage,
+  HeroImage1,
+  HeroImage2,
+  HeroImage3,
+  HeroImage4,
   aboutImage,
   about2Image,
   cart,
@@ -18,4 +32,10 @@ export {
   empty_wishlist,
   emptyCart,
   searchImage,
+  feed1,
+  feed2,
+  feed3,
+  feed4,
+  feed5,
+  feed6,
 };
