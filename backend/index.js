5ad98f2ccaa6c7186c9e8c04bffbbfcba3175140
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-10 21:03:01 +0545

    basic configurations

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const contactRoute = require("./routes/contactRoutes");
 
 app.use(express.json());
+
+app.use("/api/v1/contact", contactRoute);
+
+app.use(globalErrorHandler);
 
 module.exports = app;
