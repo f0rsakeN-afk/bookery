cec6228e26087f37677687aa577ebf1222454430
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-10 21:06:58 +0545

    fix contact

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -5,7 +5,7 @@ exports.sendMessage = catchAsync(async (req, res, next) => {
   const contactMessage = await Contact.create(req.body);
   res.status(201).json({
     status: "success",
-    data: message,
+    data: contactMessage,
   });
 });
 
