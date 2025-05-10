e2ee0216080fb6926b9bb2d91e704134f8c1243a
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-10 21:02:46 +0545

    added db connecttion

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,35 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+
+if (!DATABASE_PASSWORD || !DATABASE_URL) {
+  console.log("No DB urls found", 404);
+  process.exit(1);
+}
+
+const DB = DATABASE_URL.replace("<db_password>", DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB)
+  .then((con) => {
+    /*     console.log(con.connections) */
+    console.log("Database connection successfull!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("unhandled rejection error");
+  server.close(() => {
+    process.exit(1);
+  });
 });
