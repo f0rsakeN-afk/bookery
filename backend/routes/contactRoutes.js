08b7324447f7e5b8883da83ff0ecfe0727eb8529
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-10 21:02:20 +0545

    Added contact routes

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,7 +1,10 @@
-const express=require('express')
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const contactController = require("../controllers/contactController");
 
+router
+  .route("/")
+  .post(contactController.sendMessage)
+  .get(contactController.getAllMessages);
 
-
-
-module.exports=router;
+module.exports = router;
