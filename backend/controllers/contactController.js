e23bf10800a606dcf9598ed454c2210247f192b2
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-10 21:01:47 +0545

    added contactController

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,6 +1,21 @@
-const Contact=require('../models/contactModel')
-const catchAsync=require('../controllers/catchAsync.js')
+const Contact = require("../models/contactModel");
+const catchAsync = require("../utils/catchAsync");
 
-exports.sendMessage=catchAsync(async(req,res,next)=>{
-  
-})
+exports.sendMessage = catchAsync(async (req, res, next) => {
+  const contactMessage = await Contact.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      message: contactMessage,
+    },
+  });
+});
+
+exports.getAllMessages = catchAsync(async (req, res, next) => {
+  const messages = await Contact.find();
+  res.status(200).json({
+    status: "success",
+    results: messages.length,
+    data: messages,
+  });
+});
---
+++ backend/controllers/errorController.js
@@ -0,0 +1,35 @@
+const AppError = require("../utils/appError");
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: "Failed",
+      message: "Something went very wrong!!",
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  this.status = this.status || "Fail";
+  this.statusCode = this.statusCode || 500;
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = Object.create(err);
+    sendErrorProd(error, res);
+  }
+};
