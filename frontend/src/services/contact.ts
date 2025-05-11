1b2cdc123243beb929ff7c1943c0e564f8133410
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-11 08:46:40 +0545

    backend integration done

--- frontend/src/services/contact.ts
+++ frontend/src/services/contact.ts
@@ -1,25 +1,45 @@
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "./axios";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { toast } from "sonner";
-import { ContactProps, ContactResponse } from "@/types/contact";
+import {
+  ContactProps,
+  ContactResponse,
+  getAllMessagesResponse,
+} from "@/types/contact";
 
 async function contact(data: ContactProps): Promise<ContactResponse> {
-  const response = await axiosInstance.post(``, data);
+  const response = await axiosInstance.post(`contact/`, data);
   return response.data;
 }
 
 export function useContact() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   return useMutation<ContactResponse, AxiosError, ContactProps>({
     mutationFn: contact,
     onSuccess: (data) => {
-      toast(data.message || "Form submitted successfully");
+      toast.success(data.message || "Form submitted successfully");
+      queryClient.invalidateQueries({ queryKey: ["getAllMessages"] });
       navigate("/");
     },
     onError: (error) => {
-      toast(error.message || "Form submission failed");
+      toast.error(error.message || "Form submission failed");
     },
+  });
+}
+
+async function getAllMessages(): Promise<getAllMessagesResponse> {
+  const response = await axiosInstance.get<getAllMessagesResponse>(`contact/`);
+  return response.data;
+}
+
+export function useGetAllMessages() {
+  return useQuery<getAllMessagesResponse>({
+    queryKey: ["getAllMessages"],
+    queryFn: getAllMessages,
+    retry: 2,
+    staleTime: 60000,
   });
 }
