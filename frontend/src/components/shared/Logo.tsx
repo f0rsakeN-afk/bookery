9bbd21eba4527af59a9e75660b221670b505d5ac
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-11 08:47:52 +0545

    added app type  beta

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -4,8 +4,9 @@ import { NavLink } from "react-router-dom";
 const Logo = () => {
   return (
     <div className="h-10 w-28 flex items-center">
-      <NavLink to='/' >
-      <img src={LogoImage} alt="logo image" className=""  />
+      <NavLink to="/" className="flex items-center space-x-1">
+        <img src={LogoImage} alt="logo image" className="h-6" />
+        <span className="text-xs font-semibold text-gray-500">BETA</span>
       </NavLink>
     </div>
   );
