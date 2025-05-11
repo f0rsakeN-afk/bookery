61f239b3d277504bd8259b05428b6713df960eae
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-12 00:06:19 +0545

    pagination remain

--- frontend/src/pages/AllProducts.tsx
+++ frontend/src/pages/AllProducts.tsx
@@ -8,10 +8,16 @@ import {
   PaginationLink,
   PaginationNext,
 } from "@/components/ui/pagination";
+import { useGetAllProducts } from "@/services/product";
+import Loader from "@/components/shared/Loader";
+import ProductTile from "@/components/shared/ProductTile";
 
 const AllProducts: React.FC = () => {
+  const { data: productData, isLoading } = useGetAllProducts();
+  if (isLoading) return <Loader />;
+
   return (
-    <div className="container mx-auto px-2 xl:px-0 max-w-6xl py-8 md:py-10">
+    <div className="container mx-auto px-4 xl:px-0 max-w-6xl py-6">
       <motion.h1
         className="text-3xl font-bold flex items-center gap-2 font-playfair text-primary/90"
         initial={{ y: -20 }}
@@ -20,29 +26,32 @@ const AllProducts: React.FC = () => {
       >
         All Products
       </motion.h1>
-      <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href="#" />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">2</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">3</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">4</PaginationLink>
-          </PaginationItem>
 
-          <PaginationItem>
-            <PaginationNext href="#" />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+      <section
+        className="grid gap-6 mt-6 [grid-template-columns:repeat(auto-fit,minmax(220px,1fr))]"
+      >
+        {productData?.data?.map((el) => (
+          <ProductTile el={el} key={el.id} />
+        ))}
+      </section>
+
+      <div className="mt-8">
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            {[1, 2, 3, 4].map((page) => (
+              <PaginationItem key={page}>
+                <PaginationLink href="#">{page}</PaginationLink>
+              </PaginationItem>
+            ))}
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
     </div>
   );
 };
