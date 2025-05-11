3b1c41f7dc30679fdb9e910594635b916c5d1e14
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-12 00:03:37 +0545

    added product routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const productController = require("../controllers/productController");
+
+router
+  .route("/")
+  .post(productController.addProduct)
+  .get(productController.getAllProducts);
+
+router.route("/:id").get(productController.getProductById);
+
+module.exports = router;
