5c336fb91f4a980fecceff8111ea662a9337fafd
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-12 00:04:25 +0545

    Added product types

--- frontend/src/types/product.ts
+++ frontend/src/types/product.ts
@@ -1,7 +1,26 @@
-export interface productTypes{
-
+export interface productTypes {
+  _id?: string;
+  id?: string;
+  title: string;
+  description: string;
+  price: number;
+  quantity: string;
+  category: string;
+  brand: string;
+  discountPercentage: number;
+  image: string;
+  createdAt?: string;
+  updatedAt?: string;
+  priceAfterDiscount: number;
 }
 
-export interface productTypesResponse{
-  
+export interface productDetailTypes {
+  status: string;
+  data: productTypes;
+}
+
+export interface allProductTypesResponse {
+  status: string;
+  results: number;
+  data: productTypes[];
 }
