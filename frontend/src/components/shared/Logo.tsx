b62663cc7b94e3709235b42f946256b02971428a
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-11 09:16:08 +0545

    updated logo

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 const Logo = () => {
   return (
     <div className="h-10 w-28 flex items-center">
-      <NavLink to="/" className="flex items-center space-x-1">
+      <NavLink to="/" className="flex items-start space-x-1">
         <img src={LogoImage} alt="logo image" className="h-6" />
-        <span className="text-xs font-semibold text-gray-500">BETA</span>
+        <span className="text-[10px] font-semibold text-gray-500">BETA</span>
       </NavLink>
     </div>
   );
