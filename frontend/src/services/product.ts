b76f73751de7e921ca5bee6ad8b361386924ae8d
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-12 00:05:11 +0545

    added product services

--- frontend/src/services/product.ts
+++ frontend/src/services/product.ts
@@ -1,17 +1,30 @@
 import { useQuery } from "@tanstack/react-query";
 import axiosInstance from "./axios";
-import { productTypesResponse } from "@/types/product";
+import { allProductTypesResponse, productDetailTypes } from "@/types/product";
 
-async function getAllProducts(): Promise<productTypesResponse> {
-  const response = await axiosInstance.get<productTypesResponse>(``);
+async function getAllProducts(): Promise<allProductTypesResponse> {
+  const response = await axiosInstance.get<allProductTypesResponse>(`product/`);
   return response.data;
 }
 
 export function useGetAllProducts() {
-  return useQuery<productTypesResponse>({
+  return useQuery<allProductTypesResponse>({
     queryKey: ["allproducts"],
     queryFn: getAllProducts,
     refetchOnWindowFocus: true,
   });
 }
 
+async function getProductDetails(id: string): Promise<productDetailTypes> {
+  const response = await axiosInstance.get<productDetailTypes>(`product/${id}`);
+  return response.data;
+}
+
+export function useGetProductDetails(id: string) {
+  return useQuery<productDetailTypes>({
+    queryKey: ["productDetails", id],
+    queryFn: () => getProductDetails(id),
+    enabled: !!id,
+    refetchOnWindowFocus: true,
+  });
+}
