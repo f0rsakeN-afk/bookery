68d83112120a324d54d44c6251e53785759a8a02
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-13 10:15:52 +0545

    added product list services

---
+++ frontend/src/services/productList.ts
@@ -0,0 +1,15 @@
+import { allProductTypesResponse } from "@/types/product";
+import axiosInstance from "./axios";
+import { useQuery } from "@tanstack/react-query";
+
+async function getAllProducts(): Promise<allProductTypesResponse> {
+  const response = await axiosInstance.get<allProductTypesResponse>(`product/?limit=12`);
+  return response.data;
+}
+
+export function useGetAllProducts() {
+  return useQuery<allProductTypesResponse>({
+    queryKey: ["dashboardAllProducts"],
+    queryFn: getAllProducts,
+  });
+}
