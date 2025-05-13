6d38230a69ed92d86efab7c618d61f4f5214f51f
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-13 10:17:36 +0545

    Added types for products

--- frontend/src/types/product.ts
+++ frontend/src/types/product.ts
@@ -4,9 +4,9 @@ export interface productTypes {
   title: string;
   description: string;
   price: number;
-  quantity: string;
-  category: string;
-  brand: string;
+  quantity?: string;
+  category?: string;
+  brand?: string;
   discountPercentage: number;
   image: string;
   createdAt?: string;
@@ -23,4 +23,16 @@ export interface allProductTypesResponse {
   status: string;
   results: number;
   data: productTypes[];
+  pagination: {
+    totalItems: number;
+    totalPages: number;
+    page: number;
+    limit: 8;
+  };
 }
+/*  "pagination": {
+        "totalItems": 9,
+        "totalPages": 2,
+        "page": 1,
+        "limit": 8
+    } */
