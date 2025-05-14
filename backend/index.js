1b2927daacbd457a47482c549f578a63cb143da0
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-15 00:19:12 +0545

    Added user routes

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bodyParser=require('body-parser')
+const bodyParser = require("body-parser");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const contactRoute = require("./routes/contactRoutes");
 const productRoute = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 /* app.use(express.json()); */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.use(
   cors({
@@ -23,6 +23,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("development"));
 }
 
+app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/contact", contactRoute);
 app.use("/api/v1/product", productRoute);
 
