b49abbc6234863c25972d098736861ec0b513b8d
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-14 15:28:24 +0545

    added product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/product");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
+const APIFeatures = require("../utils/apiFeatures");
 
 exports.addProduct = catchAsync(async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -12,11 +13,30 @@ exports.addProduct = catchAsync(async (req, res, next) => {
 });
 
 exports.getAllProducts = catchAsync(async (req, res, next) => {
-  const products = await Product.find().sort("-createdAt");
+  const filteredQuery = new APIFeatures(Product.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields();
+
+  const totalItems = await filteredQuery.query.clone().countDocuments();
+
+  const paginatedQuery = filteredQuery.paginate();
+  const products = await paginatedQuery.query;
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 12;
+  const totalPages = Math.ceil(totalItems / limit);
+
   res.status(200).json({
     status: "success",
     results: products.length,
     data: products,
+    pagination: {
+      totalItems,
+      totalPages,
+      page,
+      limit,
+    },
   });
 });
 
@@ -28,5 +48,34 @@ exports.getProductById = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: "success",
     data: product,
+  });
+});
+
+exports.searchProducts = catchAsync(async (req, res, next) => {
+  const search = req.query.search;
+
+  let query = {};
+  if (search) {
+    const regex = new RegExp(search, "i");
+    query = {
+      $or: [
+        { name: { $regex: regex } },
+        { category: { $regex: regex } },
+        { description: { $regex: regex } },
+      ],
+    };
+  }
+
+  const products = await Product.find(query).select(
+    "title priceAfterDiscount price discountPercentage image id"
+  );
+
+  /* if (products.length===0) {
+    return next(new AppError("No products found", 404));
+  } */
+  res.status(200).json({
+    status: "success",
+    results: products.length,
+    data: products,
   });
 });
