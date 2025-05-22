ede57f95b9a6c7cc3bb525f6cb3923a8b9ead8c3
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-22 22:06:05 +0545

    added express mongo sanitizE

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
 const bodyParser = require("body-parser");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -17,6 +18,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(helmet());
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("development"));
+}
+
 app.use(cookieParser());
 
 const limiter = rateLimit({
@@ -27,16 +32,14 @@ const limiter = rateLimit({
 
 app.use("/api", limiter);
 
+app.use(mongoSanitize());
+
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://yourfrontend.com"],
     credentials: true,
   })
 );
-
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("development"));
-}
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/contact", contactRoute);
