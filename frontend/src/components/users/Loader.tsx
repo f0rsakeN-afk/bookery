5ac6afbd2f118017d38f0920907b1c299497c800
f0rsakeN-afk <nar3sh0@gmail.com>
2025-06-03 20:46:25 +0545

    added alert and ser loader

---
+++ frontend/src/components/users/Loader.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const UsersSkeleton: React.FC = () => {
+  return (
+    <div className="container mx-auto max-w-6xl px-4 my-10">
+      <h1 className="text-2xl sm:text-3xl font-bold font-playfair text-primary/90 mb-6">
+        All Users
+      </h1>
+
+      <div className="overflow-x-auto rounded-xl border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[200px]">Name</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Phone</TableHead>
+              <TableHead>Location</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 5 }).map((_, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <Skeleton className="h-4 w-32" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-48" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-24" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-28" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default UsersSkeleton;
