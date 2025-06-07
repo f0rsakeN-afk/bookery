45adcaf8049e2e8fab9149a85cbb060e93beb5cf
f0rsakeN-afk <nar3sh0@gmail.com>
2025-06-07 09:50:16 +0545

    added link

--- frontend/src/components/Footer/IconTile.tsx
+++ frontend/src/components/Footer/IconTile.tsx
@@ -4,6 +4,7 @@ import { FacebookIcon, InstagramIcon, Mail, Dribbble } from "lucide-react";
 interface dataTypes {
   name: string;
   icon: React.ReactNode;
+  link?: string;
 }
 
 const data: dataTypes[] = [
@@ -14,6 +15,7 @@ const data: dataTypes[] = [
   {
     name: "Instagram",
     icon: <InstagramIcon />,
+    link: "https://www.instagram.com/_f0rsak3n/",
   },
   { name: "Mail", icon: <Mail /> },
   { name: "Dribble", icon: <Dribbble /> },
@@ -22,13 +24,15 @@ const data: dataTypes[] = [
 const IconTile: React.FC = () => {
   return (
     <div className="flex gap-5 pt-2">
-      {data.map((el:dataTypes) => (
-        <span
+      {data.map((el: dataTypes) => (
+        <a
+          href={el.link}
+          target="_blank"
           className=" text-primary/70 hover:text-figmaPrimary transition-colors ease-linear duration-200 cursor-pointer"
           key={el.name}
         >
           {el.icon}
-        </span>
+        </a>
       ))}
     </div>
   );
