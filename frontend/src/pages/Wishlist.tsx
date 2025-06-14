593a957d654bf29a5c6d4817d82ebafb22d34f58
f0rsakeN-afk <nar3sh0@gmail.com>
2025-06-14 08:39:32 +0545

    Added wishlist page

--- frontend/src/pages/Wishlist.tsx
+++ frontend/src/pages/Wishlist.tsx
@@ -2,29 +2,30 @@ import { motion } from "motion/react";
 import WishlistHeader from "@/components/wishlist/WishlistHeader";
 import WishlistTable from "@/components/wishlist/WishlistItems";
 import EmptyWishlist from "@/components/wishlist/EmptyWishlist";
+import { useGetWishlist } from "@/services/wishlist";
+import { Button } from "@/components/ui/button";
+import WishlistTableSkeleton from "@/components/wishlist/WishListLoader";
 
 const Wishlist = () => {
-  const wishlistItems = [
-    {
-      id: 1,
-      title: "The Great Gatsby",
-      author: "F. Scott Fitzgerald",
-      price: 19.99,
-      image:
-        "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
-      inStock: true,
-    },
-    {
-      id: 2,
-      title: "The Great Gatsbyc  cdsv",
-      author: "F. Scott Fitzgerald",
-      price: 19.99,
-      image:
-        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
-      inStock: true,
-    },
-    // Add more items...
-  ];
+  const { data: wishListData, isLoading, isError, refetch } = useGetWishlist();
+
+  if (isLoading) return <WishlistTableSkeleton />;
+  /*   console.log(wishListData); */
+
+  if (isError)
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.4 }}
+        className="flex flex-col items-center justify-center px-4 py-12 text-center"
+      >
+        <p className="text-sm text-muted-foreground font-medium font-inter mb-4">
+          Oops! We couldn't load your wishlist. Please try again.
+        </p>
+        <Button onClick={() => refetch()}>Retry</Button>
+      </motion.div>
+    );
 
   return (
     <motion.div
@@ -33,9 +34,9 @@ const Wishlist = () => {
       transition={{ duration: 0.5 }}
       className="container mx-auto max-w-6xl px-4 xl:px-0 xl:py-8"
     >
-      <WishlistHeader itemCount={wishlistItems.length} />
-      {wishlistItems.length > 0 ? (
-        <WishlistTable items={wishlistItems} />
+      <WishlistHeader itemCount={wishListData.results} />
+      {wishListData.results > 0 ? (
+        <WishlistTable items={wishListData.data} />
       ) : (
         <EmptyWishlist />
       )}
