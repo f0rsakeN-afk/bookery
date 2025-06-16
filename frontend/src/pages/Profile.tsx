1d56700329efabe6c3d194b0972c6ac6480ddb6f
f0rsakeN-afk <nar3sh0@gmail.com>
2025-06-16 07:53:16 +0545

    added new tab

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 import { motion } from "motion/react";
-import {  Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import UserDetailsForm from "@/components/profile/UserDetailsForm";
 import PasswordForm from "@/components/profile/PasswordForm";
+import { useAuth } from "@/context/AuthContext";
+import { cn } from "@/lib/utils";
+import UserOrderHistory from "@/components/profile/UserOrderHistory";
 
 const Profile: React.FC = () => {
+  const { user } = useAuth();
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      className="container mx-auto max-w-6xl px-2 xl:px-0 py-10"
+      className="container mx-auto max-w-6xl px-2 xl:px-0 py-4 xl:py-6"
     >
       <Tabs defaultValue="details" className="w-full">
         <TabsList className="flex justify-center items-center space-x-4">
           <TabsTrigger value="details">User Details</TabsTrigger>
+          <TabsTrigger
+            value="orderhistory"
+            className={cn(user && user.role === "admin" && "hidden")}
+          >
+            Order History
+          </TabsTrigger>
           <TabsTrigger value="password">Change Password</TabsTrigger>
         </TabsList>
 
@@ -36,6 +46,20 @@ const Profile: React.FC = () => {
             </CardHeader>
             <CardContent>
               <PasswordForm />
+            </CardContent>
+          </Card>
+        </TabsContent>
+
+        <TabsContent
+          value="orderhistory"
+          className={cn(user && user.role === "admin" && "hidden")}
+        >
+          <Card>
+            <CardHeader>
+              <CardTitle>Order History</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <UserOrderHistory />
             </CardContent>
           </Card>
         </TabsContent>
