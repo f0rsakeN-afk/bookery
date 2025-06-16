695b689c2236db88108f47cc9bdd85651442a2fc
f0rsakeN-afk <nar3sh0@gmail.com>
2025-06-16 07:54:55 +0545

    added update user info func

--- frontend/src/components/profile/UserDetailsForm.tsx
+++ frontend/src/components/profile/UserDetailsForm.tsx
@@ -1,33 +1,86 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { motion } from "motion/react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useAuth } from "@/context/AuthContext";
+import { useGetUserDetails, useUpdateUserInfo } from "@/services/user";
+import Loader from "../shared/Loader";
 
 interface UserDetailsFormData {
   firstName: string;
-  lastName: string;
   email: string;
   phone: string;
   location: string;
   bio: string;
   occupation: string;
   company: string;
-  timezone: string;
+  timeZone: string;
   language: string;
-  role: string;
 }
 
 const UserDetailsForm: React.FC = () => {
-  const { register, handleSubmit, /* formState: { errors } */ } = useForm<UserDetailsFormData>();
+  const { user } = useAuth();
+  const { data, isLoading } = useGetUserDetails(user?._id!);
+  /*   console.log(data); */
+  const useUpdateUserMutation = useUpdateUserInfo();
 
-  const onSubmit = (data: UserDetailsFormData) => {
-    console.log(data);
-    // Handle form submission
+  const { register, handleSubmit, control } = useForm<UserDetailsFormData>({
+    defaultValues: {
+      firstName: data?.data.name || "",
+      email: data?.data.email || "",
+      phone: data?.data.phone || "",
+      location: data?.data.location || "",
+      bio: data?.data.bio || "",
+      occupation: data?.data.occupation || "",
+      company: data?.data.company || "",
+      timeZone: data?.data.timeZone || "",
+      language: data?.data.language || "",
+    },
+    values: {
+      firstName: data?.data.name || "",
+      email: data?.data.email || "",
+      phone: data?.data.phone || "",
+      location: data?.data.location || "",
+      bio: data?.data.bio || "",
+      occupation: data?.data.occupation || "",
+      company: data?.data.company || "",
+      timeZone: data?.data.timeZone || "",
+      language: data?.data.language || "",
+    },
+  });
+
+  if (isLoading) return <Loader />;
+
+  const onSubmit = (formData: UserDetailsFormData) => {
+    if (!user) return;
+    const { phone, location, bio, occupation, company, timeZone, language } =
+      formData;
+
+    useUpdateUserMutation.mutate({
+      id: user._id!,
+      data: {
+        phone,
+        location,
+        bio,
+        occupation,
+        company,
+        timeZone,
+        language,
+      },
+    });
   };
+
+  const isPending = useUpdateUserMutation.isPending;
 
   return (
     <motion.form
@@ -40,79 +93,95 @@ const UserDetailsForm: React.FC = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="space-y-2">
           <Label htmlFor="firstName">First Name</Label>
-          <Input {...register("firstName")} placeholder="Enter first name" />
-        </div>
-
-        <div className="space-y-2">
-          <Label htmlFor="lastName">Last Name</Label>
-          <Input {...register("lastName")} placeholder="Enter last name" />
+          <Input {...register("firstName")} disabled readOnly />
         </div>
 
         <div className="space-y-2">
           <Label htmlFor="email">Email</Label>
-          <Input {...register("email")} type="email" placeholder="Enter email" />
+          <Input {...register("email")} type="email" disabled readOnly />
         </div>
 
         <div className="space-y-2">
           <Label htmlFor="phone">Phone Number</Label>
-          <Input {...register("phone")} placeholder="Enter phone number" />
+          <Input {...register("phone")} disabled={isPending} />
         </div>
 
         <div className="space-y-2">
           <Label htmlFor="location">Location</Label>
-          <Input {...register("location")} placeholder="Enter location" />
+          <Input {...register("location")} disabled={isPending} />
         </div>
 
         <div className="space-y-2">
           <Label htmlFor="occupation">Occupation</Label>
-          <Input {...register("occupation")} placeholder="Enter occupation" />
+          <Input {...register("occupation")} disabled={isPending} />
         </div>
 
         <div className="space-y-2">
           <Label htmlFor="company">Company</Label>
-          <Input {...register("company")} placeholder="Enter company name" />
+          <Input {...register("company")} disabled={isPending} />
         </div>
-
-      
 
         <div className="space-y-2">
           <Label htmlFor="language">Preferred Language</Label>
-          <Select>
-            <SelectTrigger>
-              <SelectValue placeholder="Select language" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="en">English</SelectItem>
-              <SelectItem value="es">Spanish</SelectItem>
-              <SelectItem value="fr">French</SelectItem>
-            </SelectContent>
-          </Select>
+          <Controller
+            name="language"
+            control={control}
+            render={({ field }) => (
+              <Select
+                disabled={isPending}
+                onValueChange={field.onChange}
+                value={field.value}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select language" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="en">English</SelectItem>
+                  <SelectItem value="nep">Nepali</SelectItem>
+                  <SelectItem value="es">Spanish</SelectItem>
+                  <SelectItem value="fr">French</SelectItem>
+                </SelectContent>
+              </Select>
+            )}
+          />
         </div>
 
         <div className="space-y-2">
           <Label htmlFor="timezone">Timezone</Label>
-          <Select>
-            <SelectTrigger>
-              <SelectValue placeholder="Select timezone" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="utc">UTC</SelectItem>
-              <SelectItem value="est">EST</SelectItem>
-              <SelectItem value="pst">PST</SelectItem>
-            </SelectContent>
-          </Select>
+          <Controller
+            name="timeZone"
+            control={control}
+            render={({ field }) => (
+              <Select
+                disabled={isPending}
+                onValueChange={field.onChange}
+                value={field.value}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select timezone" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="utc">UTC</SelectItem>
+                  <SelectItem value="est">EST</SelectItem>
+                  <SelectItem value="pst">PST</SelectItem>
+                </SelectContent>
+              </Select>
+            )}
+          />
         </div>
       </div>
 
       <div className="space-y-2">
         <Label htmlFor="bio">Bio</Label>
-        <Textarea {...register("bio")} placeholder="Tell us about yourself" />
+        <Textarea
+          {...register("bio")}
+          className="max-h-40"
+          disabled={isPending}
+        />
       </div>
 
-     
-
-      <Button type="submit" className="w-full md:w-auto">
-        Save Changes
+      <Button type="submit" className="w-full md:w-auto" disabled={isPending}>
+        {isPending ? "Saving..." : "Save Info"}
       </Button>
     </motion.form>
   );
